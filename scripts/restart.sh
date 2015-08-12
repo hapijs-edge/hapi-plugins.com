4e2ea0c676cef2ab202b0574fdf8dde192a7588e
Van Nguyen <thegoleffect@gmail.com>
2015-08-12 09:16:07 -0700

    add github button and goog analytics

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
-kill -SIGINT `cat .pid`
-PRODUCTION=1 nohup node -max-old-space-size=8192 index.js &
+# kill -SIGINT `cat .pid`
+killall node
+# PRODUCTION=1 nohup node -max-old-space-size=8192 index.js &
+PRODUCTION=1 forever start -a --uid "hapiplugins" --killSignal=SIGTERM -c node index.js
 echo $! > .pid
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
-PRODUCTION=1 nohup node -max-old-space-size=8192 index.js &
+PRODUCTION=1 forever start -a --uid "hapiplugins" --killSignal=SIGTERM -c node index.js
+# PRODUCTION=1 nohup node -max-old-space-size=8192 index.js &
 echo $! > .pid
